d4f80e536aad91e4519c9898da47d50b125958cb
Wenter <uuooooq@gmail.com>
2019-10-10 09:23:45 +0800

    Initial commit

---
+++ mingyan/mingyan/spiders/mingyan_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class mingyan(scrapy.Spider):
+    name = "mingyan2"
+
+    def start_requests(self):
+        
+        urls = [
+            'http://lab.scrapyd.cn/page/1/',
+            'http://lab.scrapyd.cn/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        
+        page = response.url.split("/")[-2]
+        filename = 'mingyan-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('保存文件:%s' % filename)
