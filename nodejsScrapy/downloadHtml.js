c481b54643484d48bec796b4e4aa33c146bcbe02
Wenter <uuooooq@gmail.com>
2019-10-20 22:59:48 +0800

    开始解析html

--- nodejsScrapy/downloadHtml.js
+++ nodejsScrapy/downloadHtml.js
@@ -11,20 +11,65 @@ let url = "https://www.jjxsw.la/";
 
 let urlType = url.startsWith("https") ? https : http;
 
-urlType.get(url, (res) => {
-    var content = "";
 
-    //获取数据
-    res.on("data", buf => {
-        content += buf;
-    });
+fs.exists('./target.html', function(exists){
+    if(exists){
+        fs.readFile('./target.html','utf-8', (err, data) => {
+            var con = cheerio.load(data);
+            //var copyright = con.finc('.new').text;
+            //var lesson_list = con('.subnav').find('ul').children('li').text();
+            //var title = con('.searchbut').attr('title');
+            var novellist = con('.iArticle').find('li a');
+            for (const key in novellist) {
+                if (novellist.hasOwnProperty(key)) {
+                    const element = novellist[key];
+                    console.log('title:'+ element.attribs['title']+'  url:'+element.attribs['href']);
+                    
+                }
+            }
+            //console.log('title:'+novellist);
+        })
 
-    res.on("end", ()=>{
-        //console.log(content);
-        var con = cheerio.load(content);
-        //var copyright = con.finc('.new').text;
-        var lesson_list = con('.subnav').find('ul').children('li').text;
-        console.log(lesson_list);
-    })
+    }else{
+        urlType.get(url, (res) => {
+            var content = "";
+        
+            //获取数据
+            res.on("data", buf => {
+                content += buf;
+            });
+        
+            res.on("end", ()=>{
+                // //console.log(content);
+                // var con = cheerio.load(content);
+                // //var copyright = con.finc('.new').text;
+                // //var lesson_list = con('.subnav').find('ul').children('li').text();
+                // //var title = con('.searchbut').attr('title');
+                // var novellist = con.find('li a').attr('title');
+                // console.log('title:'+novellist);
+                fs.writeFileSync("./target.html", content);
+            })
+        })
+    }
 })
+
+
+// urlType.get(url, (res) => {
+//     var content = "";
+
+//     //获取数据
+//     res.on("data", buf => {
+//         content += buf;
+//     });
+
+//     res.on("end", ()=>{
+//         //console.log(content);
+//         var con = cheerio.load(content);
+//         //var copyright = con.finc('.new').text;
+//         //var lesson_list = con('.subnav').find('ul').children('li').text();
+//         //var title = con('.searchbut').attr('title');
+//         var novellist = con.find('li a').attr('title');
+//         console.log('title:'+novellist);
+//     })
+// })
 
---
+++ nodejsScrapy/xixi.js
@@ -0,0 +1,6 @@
+console.log("hello xixi");
+var v = 1 + 2 +3 +4;
+
+console.log("1 + 2 +3 +4 = "+ v);
+
+     
